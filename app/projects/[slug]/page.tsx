e49717a86fd3400ca61b74a705fd67ee33bc6298
Giovanni Aguirre <gaguirre@2600hz.com>
2026-05-12 22:32:22 -0600

    upgrade next to 16 and react to 19

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -11,8 +11,9 @@ export async function generateStaticParams() {
   }));
 }
 
-export function generateMetadata({ params }) {
-  let post = getProjectsPosts().find((post) => post.slug === params.slug);
+export async function generateMetadata({ params }) {
+  const { slug } = await params;
+  let post = getProjectsPosts().find((post) => post.slug === slug);
   if (!post) {
     return;
   }
@@ -51,8 +52,9 @@ export function generateMetadata({ params }) {
   };
 }
 
-export default function Project({ params }) {
-  let post = getProjectsPosts().find((post) => post.slug === params.slug);
+export default async function Project({ params }) {
+  const { slug } = await params;
+  let post = getProjectsPosts().find((post) => post.slug === slug);
 
   if (!post) {
     notFound();
